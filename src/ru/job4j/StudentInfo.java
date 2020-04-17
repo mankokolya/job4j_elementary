65f82f10e64ebca79bb809e207e68eaa9a86a377
manko <mankokolya@gmail.com>
2020-04-17 22:52:21 +0300

    3.3. Разбор первой программы.[#275418]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Mykola Manko");
+        System.out.println("14.12.1990");
+    }
+}
