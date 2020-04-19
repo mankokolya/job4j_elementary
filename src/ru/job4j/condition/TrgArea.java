882985ca36a078114792276ad2780bf0eb5a0c7b
manko <mankokolya@gmail.com>
2020-04-19 20:33:34 +0300

    Checked with checkstyle 9. Площадь треугольника.[#275405]

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -1,13 +1,13 @@
 package ru.job4j.condition;
 
 public class TrgArea {
-    public static double area(double a, double b, double c){
-        double halfPerimeter = (a + b+ c) / 2;
+    public static double area(double a, double b, double c) {
+        double halfPerimeter = (a + b + c) / 2;
         return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
     }
 
     public static void main(String[] args) {
-        double trgArea = TrgArea.area(2,2,2);
+        double trgArea = TrgArea.area(2, 2, 2);
         System.out.println("The area of the triangle with sides (2, 2, 2) is " + trgArea);
     }
 }
