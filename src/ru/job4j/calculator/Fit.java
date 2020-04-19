6e745d20ad9f66701bbad40c88a53d1f8c18b07f
manko <mankokolya@gmail.com>
2020-04-19 10:33:39 +0300

    6. Идеальный вес.[#275399]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(double height) {
+         return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(double height) {
+        return (height - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        double manIdealWeight = Fit.manWeight(180);
+        System.out.println("Ideal weight of 180 centimeters tall man is " + manIdealWeight + " kilograms.");
+
+        double womanIdealWeight = Fit.womanWeight(160);
+        System.out.println("Ideal weight of 160 centimeters tall woman is " + womanIdealWeight + " kilograms.");
+
+    }
+}
