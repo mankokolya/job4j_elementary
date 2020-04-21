91e3d7ee9bd3f401301147d1b01cf24ca2f2bf3c
Mykola Manko <mankokolya@gmail.com>
2020-04-21 15:31:34 +0300

    5.4. Протеиновая диета.[#275448]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calcTime(int ivan, int nick){
+        int month = 0;
+        while(ivan < nick){
+            ivan *= 3;
+            nick *= 2;
+            month++;
+        }
+        return month;
+    }
+}
