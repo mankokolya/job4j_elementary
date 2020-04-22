e86dcfbe932b70a23dddce513f17cf642fc0e812
manko <mankokolya@gmail.com>
2020-04-22 20:42:31 +0300

    5.4. Протеиновая диета.[#275448] I added a new test when starting weight is equal and corrected the condition in the while loop.

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -1,9 +1,9 @@
 package ru.job4j.loop;
 
 public class Fitness {
-    public static int calcTime(int ivan, int nick){
+    public static int calcTime(int ivan, int nick) {
         int month = 0;
-        while(ivan < nick){
+        while (ivan <= nick) {
             ivan *= 3;
             nick *= 2;
             month++;
