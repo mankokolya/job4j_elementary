fb05777f20db8d86fea2fdb78f51b2762279fe8e
manko <mankokolya@gmail.com>
2020-04-25 18:52:05 +0300

    6.5.3. Сортировка выборкой.[#275381] Changed swap of the elements in the array with the help of the existed class SwitchArray;

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -1,24 +1,21 @@
 package ru.job4j.array;
 
 /**
- *  SortedSelected - is used to sort numbers in the array selecting the minimum number in the array and shifting
- *  it to the beginning in increasing order;
+ * SortedSelected - is used to sort numbers in the array selecting the minimum number in the array and shifting
+ * it to the beginning in increasing order;
  *
  * @author mankokolya;
  */
 public class SortSelected {
     /**
-     *
      * @param data - provedid array that need to be sorted in increasing order;
-     *
      * @return - sorted array;
      */
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinNumberInRange.findMinInRange(data, i, data.length - 1);
             int index = FindLoop.indexOfElement(data, min, i, data.length - 1);
-            data[index] = data[i];
-            data[i] = min;
+            SwitchArray.swap(data, i, index);
         }
         return data;
     }
