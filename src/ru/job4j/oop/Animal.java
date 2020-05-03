b9fe88f5bb329611afe2a0ba0bb3503feb886a71
manko <mankokolya@gmail.com>
2020-05-03 22:55:43 +0300

    1. Конструктор по умолчанию.[#275546]

---
+++ src/ru/job4j/oop/Animal.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class Animal {
+    public Animal() {
+        System.out.println("java.lang.Object");
+    }
+}
---
+++ src/ru/job4j/oop/Tiger.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Tiger extends Predator {
+
+    public Tiger() {
+        super();
+        System.out.println("ru.job4j.oop.Predator");
+    }
+}
