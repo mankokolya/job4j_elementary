c7c58a2c52288ca506bfc16cff4628f80a9cd47c
Mykola Manko <mankokolya@gmail.com>
2020-05-07 15:12:09 +0300

    2. Scanner и чтение числа из консоли.[#275497]

---
+++ src/ru/job4j/io/Matches.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        Player firstPlayer = new Player(setName());
+        Player secondPlayer = new Player(setName());
+        int matches = 11;
+        while(matches > 0) {
+            System.out.println("Welcome to the game!!!");
+            matches = firstPlayer.makeMove(matches, firstPlayer.getName());
+            checkWinner(matches, firstPlayer.getName());
+            matches = secondPlayer.makeMove(matches, secondPlayer.getName());
+            checkWinner(matches, secondPlayer.getName());
+        }
+    }
+
+    private static String setName() {
+        System.out.println("Enter your nickname: ");
+        return new Scanner(System.in).next();
+    }
+
+    private static void checkWinner(int matches, String name) {
+        if (matches <= 0) {
+            System.out.println(name + " wins!!!");
+        }
+    }
+}
---
+++ src/ru/job4j/io/Player.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int makeMove(int totalNumber, String name) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("It's " + name + "'s turn.");
+        System.out.println("Please enter a number from 1 to 3: ");
+        int move = Integer.parseInt(input.nextLine());
+        while(move <= 0 || move > 3) {
+            System.out.println("Illegal move!");
+            System.out.println("Please enter a number from 1 to 3");
+            move = Integer.parseInt(input.nextLine());
+        }
+        System.out.println((totalNumber - move) + " - matches left;");
+        return totalNumber - move;
+    }
+}
