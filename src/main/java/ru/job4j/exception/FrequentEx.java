ed3350158de24ab47940de3b6765677a1986dfb4
Nick <mankokolya@gmail.com>
2020-05-18 13:25:04 +0300

    0. Что такое исключение.[#275522]

---
+++ src/main/java/ru/job4j/exception/FrequentEx.java
@@ -0,0 +1,10 @@
+package ru.job4j.exception;
+
+public class FrequentEx {
+    public static void main(String[] args) {
+        String[] shops = {"Ebay", "Amazon", "Ozon"};
+        for (int i = 0; i < shops.length; i++) {
+            System.out.println(shops[i]);
+        }
+    }
+}
