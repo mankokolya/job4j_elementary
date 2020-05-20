1f2a3518e219f738266fe2716e785b6317d905b9
Nick <mankokolya@gmail.com>
2020-05-20 09:55:53 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#275512]

--- test/ru/job4j/tracker/ValidateInputTest.java
+++ test/ru/job4j/tracker/ValidateInputTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,29 +11,31 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class ValidateInputTest {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream def = System.out;
 
-    @Test
-    public void whenInvalidInput() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintStream def = System.out;
+    @Before
+    public void loadOutput() {
         System.setOut(new PrintStream(out));
+    }
 
-        ValidateInput input = new ValidateInput(new StableInput(new String[]{"one", "1"}));
-        input.askInt("Enter");
-        assertThat(new String(out.toByteArray()), is(String.format("Please enter a valid data again.%n")));
+    @After
+    public void returnStandardOutput() {
         System.setOut(def);
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void whenOutOfRangeInput()  {
-//        ByteArrayOutputStream out = new ByteArrayOutputStream();
-//        PrintStream def = System.out;
-//        System.setOut(new PrintStream(out));
+    @Test
+    public void whenInvalidInput() {
+        ValidateInput input = new ValidateInput(new StableInput(new String[]{"one", "1"}));
+        input.askInt("Enter");
+        assertThat(new String(out.toByteArray()), is(String.format("Please enter a valid data again.%n")));
+    }
 
-        String[] data = {"8"};
+    @Test
+    public void whenOutOfRangeInput() {
+        String[] data = {"8", "6"};
         ValidateInput input = new ValidateInput(new StableInput(data));
         input.askInt("Enter", 7);
-//        assertThat(new String(out.toByteArray()), is(String.format("Please select key from menu.%n")));
-//        System.setOut(def);
+        assertThat(new String(out.toByteArray()), is(String.format("Please select key from menu.%n")));
     }
 }
