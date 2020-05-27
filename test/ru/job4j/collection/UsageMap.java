5e67abf33bd149acce30aafc0eaa532769e5acdf
Nick <mankokolya@gmail.com>
2020-05-27 14:16:06 +0300

    0. Map, HashMap[212652#275483]

---
+++ test/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("mankokolya@gmail.com", "Mykola Manko");
+        map.put("bereza_ira@gmail.com", "Iryna Bereza");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
