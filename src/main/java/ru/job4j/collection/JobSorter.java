72161ead266f92170cfecfd1850d7d321faa0ee5
Nick <mankokolya@gmail.com>
2020-05-28 10:02:54 +0300

    Job and JobSorter

---
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+
+public class JobSorter {
+    public static void main(String[] args) {
+        List<Job> jobs = Arrays.asList(new Job("Fix bugs", 4),
+                new Job("Impl task", 2),
+                new Job("Reboot server", 1)
+        );
+        System.out.println(jobs);
+        Collections.sort(jobs);
+        System.out.println(jobs);
+        Collections.sort(jobs, new SortByNameJob());
+        System.out.println(jobs);
+    }
+}
