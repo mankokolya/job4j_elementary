1b5509572b1dc47890ceec357f1bfa45b2bc898a
manko <mankokolya@gmail.com>
2020-06-03 21:55:34 +0300

    Отсортировать департаменты[34352#275466] Modified DepartmentsSortDescOrder class

--- src/main/java/ru/job4j/collection/DepartmentsSortDescOrder.java
+++ src/main/java/ru/job4j/collection/DepartmentsSortDescOrder.java
@@ -7,20 +7,9 @@ public class DepartmentsSortDescOrder implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        int result;
         String[] o1Split = o1.split("/", 2);
         String[] o2Split = o2.split("/", 2);
-        if (o1Split.length == 1 || o2Split.length == 1) {
-            result = o2Split[0].compareTo(o1Split[0]);
-            if (result == 0) {
-                result = Integer.compare(o1Split.length, o2Split.length);
-            }
-        } else {
-            result = o2Split[0].compareTo(o1Split[0]);
-            if (result == 0) {
-                result = o1Split[1].compareTo(o2Split[1]);
-            }
-        }
-        return result;
+        int result = o2Split[0].compareTo(o1Split[0]);
+        return result == 0 ? o1.compareTo(o2) : result;
     }
 }
