d58f4838901d4398f689d4a1bf01f5a44b4633db
manko <mankokolya@gmail.com>
2020-06-11 23:58:49 +0300

    3. Уникальность элементов и сортировка.[110225#275344]

--- src/main/java/ru/job4j/lambda/Address.java
+++ src/main/java/ru/job4j/lambda/Address.java
@@ -1,6 +1,8 @@
 package ru.job4j.lambda;
 
-public class Address {
+import java.util.Objects;
+
+public class Address implements Comparable<Address> {
     private String city;
     private String street;
     private int home;
@@ -13,19 +15,32 @@ public class Address {
         this.apartment = apartment;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = false;
+        if (this == obj) {
+            result = true;
+        } else if (obj != null) {
+            if (getClass() == obj.getClass()) {
+                Address other = (Address) obj;
+                result = Objects.equals(this.city, other.city);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city);
+    }
+
     public String getCity() {
         return city;
     }
 
-    public String getStreet() {
-        return street;
-    }
 
-    public int getHome() {
-        return home;
-    }
-
-    public int getApartment() {
-        return apartment;
+    @Override
+    public int compareTo(Address o) {
+        return this.city.compareTo(o.city);
     }
 }
--- src/main/java/ru/job4j/lambda/Profiles.java
+++ src/main/java/ru/job4j/lambda/Profiles.java
@@ -2,9 +2,11 @@ package ru.job4j.lambda;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        Stream<Address> result = profiles.stream().map(Profile::getAddress);
+        return result.sorted().distinct().collect(Collectors.toList());
     }
 }
