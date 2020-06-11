cec993a667c013d9c44e56ee2b19d7821e60df2a
manko <mankokolya@gmail.com>
2020-06-11 22:51:23 +0300

    2. Список адресов[110062#275343]

---
+++ src/main/java/ru/job4j/lambda/Profile.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import java.util.List;
+
+public class Profile {
+    private Address address;
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+}
